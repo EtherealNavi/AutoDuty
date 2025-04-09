accbc6b41255058727d86438b637a93aca68ed8c
Navi <53199242+EtherealNavi@users.noreply.github.com>
2025-04-09 13:24:07 +0000

    Update LevelingHelper.cs

--- AutoDuty/Helpers/LevelingHelper.cs
+++ AutoDuty/Helpers/LevelingHelper.cs
@@ -38,6 +38,8 @@ namespace AutoDuty.Helpers
                         823u,  // Qitana
                         822u,  // Mt. Gulg
                         952u,  // Tower of Zot
+                        969u,  // Tower of Babil
+                        970u,  // Vanaspati
                         974u,  // Ktisis Hyperboreia
                         1167u, // Ihuykatumu
                         1193u, // Worqor Zormor
